d6133c9bbba3899acbf2288ab4b6ce84ed14c013
dvemolina <dvemolina@gmail.com>
2025-03-31 14:36:56 +0200

    Fix: no [lang] folder structure

---
+++ src/routes/(seo)/instructors/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import InstructorCard from "$src/features/Instructors/components/InstructorCard.svelte";
+    
+    let instructorData = {};
+</script>
+
+<InstructorCard {instructorData} />
