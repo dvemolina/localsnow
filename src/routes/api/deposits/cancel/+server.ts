3ff8934481843a60003b721a8d657bced6d3e101
Moli <dvemolina@gmail.com>
2025-11-08 06:35:31 +0800

    Delete unnecessary routes

--- src/routes/api/deposits/cancel/+server.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-    // User cancelled the payment - redirect back to homepage or search
-    throw redirect(303, '/?cancelled=true');
-};
