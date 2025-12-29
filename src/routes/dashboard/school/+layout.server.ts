5260039a7f9d4725881fd073ab8efd9364801fd6
Moli <dvemolina@gmail.com>
2025-12-29 20:24:10 +0100

    ko

--- src/routes/dashboard/school/+layout.server.ts
+++ src/routes/dashboard/school/+layout.server.ts
@@ -1,5 +1,5 @@
 import type { LayoutServerLoad } from './$types';
-import { requireSchoolAdmin } from '$lib/utils/schoolAuth';
+import { requireSchoolAdmin } from '$src/lib/utils/schoolAuth';
 
 export const load: LayoutServerLoad = async (event) => {
 	const { user, school } = await requireSchoolAdmin(event);
