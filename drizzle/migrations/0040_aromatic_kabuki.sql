34d7086fcb0cbb5dacb442f9f6c11ebb52198f2f
Moli <dvemolina@gmail.com>
2026-01-14 23:26:40 +0100

    Applied migration

---
+++ drizzle/migrations/0040_aromatic_kabuki.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "instructor_leads" ADD COLUMN "preferred_dates" text;--> statement-breakpoint
+ALTER TABLE "instructor_leads" ADD COLUMN "number_of_students" integer;--> statement-breakpoint
+ALTER TABLE "instructor_leads" ADD COLUMN "skill_level" varchar(20);
--- drizzle/migrations/0040_enhance_instructor_leads.sql
+++
@@ -1,4 +0,0 @@
--- Add lesson-related fields to instructor_leads table
-ALTER TABLE "instructor_leads" ADD COLUMN "preferred_dates" text;
-ALTER TABLE "instructor_leads" ADD COLUMN "number_of_students" integer;
-ALTER TABLE "instructor_leads" ADD COLUMN "skill_level" varchar(20);
