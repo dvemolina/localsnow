185474cf3de903e0b52236dc5edcb68c4b7f93ca
Moli <dvemolina@gmail.com>
2026-01-17 20:05:22 +0100

    user is_suspended migration fixed

--- drizzle/migrations/0045_lame_the_hand.sql
+++ drizzle/migrations/0045_lame_the_hand.sql
@@ -1,3 +1,4 @@
+-- Custom SQL migration file, put your code below! --
 ALTER TABLE "users"
 	ADD COLUMN IF NOT EXISTS "is_suspended" boolean DEFAULT false,
 	ADD COLUMN IF NOT EXISTS "suspension_reason" text,
