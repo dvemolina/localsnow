2c7d59e8d643a34278eb1a1e85e9535f02cfcbc3
Moli <dvemolina@gmail.com>
2026-01-23 19:32:14 +0100

    fix: stars of reviews to black

--- src/lib/components/shared/StarScore.svelte
+++ src/lib/components/shared/StarScore.svelte
@@ -4,5 +4,5 @@
 
 <div class="flex flex-row justify-center items-center rounded-md bg-yellow-200/75 pl-1 gap">
 	<p class="text-sm text-text font-semibold ">{score}</p>
-	<img src="/icons/star.svg" alt="Stars" class="size-4 invert-0 dark:invert"/>
+	<img src="/icons/star.svg" alt="Stars" class="size-4"/>
 </div>
